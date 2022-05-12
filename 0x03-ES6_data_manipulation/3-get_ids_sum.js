e8e9038ce0457f1c0580b76237c355f9c2cb713d
CameronCrupper <guitardude20795@yahoo.com>
2022-05-12 03:53:17 +0000

    test

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,7 +1,7 @@
 const getStudentIdsSum = (students) => {
   const everyone = students
-  .map((student) => student.id)
-  .reduce((studentPrev, studentCurrent) => studentPrev + studentCurrent);
+    .map((student) => student.id)
+    .reduce((studentPrev, studentCurrent) => studentPrev + studentCurrent);
   return everyone;
 };
 export default getStudentIdsSum;
