10734deede6abfb1174786b2834eb640759996d8
Cameron Crupper <guitardude20795@yahoo.com>
2022-08-29 17:00:02 -0500

    fixing pycode and doc

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
+"""
+Basic floor annotation
+"""
+
 
 import math
 
-def floor(n: float) -> int:
 
+def floor(n: float) -> int:
+    """
+    takes float n as arg and returns floor float
+    """
     return(math.floor(n))
