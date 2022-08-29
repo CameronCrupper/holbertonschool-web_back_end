cd6535242d24eeb33c76e172af95ee2a2a260df5
Cameron Crupper <guitardude20795@yahoo.com>
2022-08-29 17:02:25 -0500

    fixing pycode and doc

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
+"""
+basic concat annotation
+"""
+
 
 def concat(str1: str, str2: str) -> str:
-
+    """
+    TAKES STR AS ARG AND RETURN CONCAT STR
+    """
     return(str1 + str2)
