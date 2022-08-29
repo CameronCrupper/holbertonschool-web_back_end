f9233a813ab4b9675291452f83060b8071d96191
Cameron Crupper <guitardude20795@yahoo.com>
2022-08-29 16:45:52 -0500

    fixing pycode

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
+"""
+    Mixed Tuple
+"""
+from typing import Union, Tuple
 
-from typing import Tuple, Union
 
-def to_kv(k: str, v: Union[float, int]) -> Tuple[float, str]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+        Args:
+            k: String
+            v: Union: Can be int or float
+        Return:
+            Tuple with string and int or float
+    """
 
-    return (k, v * v)
+    cncat: Tuple(str, Union[int, float])
+    cncat = (k, v**2)
+
+    return cncat
