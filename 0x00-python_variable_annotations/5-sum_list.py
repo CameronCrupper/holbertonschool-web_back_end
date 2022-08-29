cb86b3ef4b90c22e4bb2dbb05dacd5705964d52f
Cameron Crupper <guitardude20795@yahoo.com>
2022-08-29 16:54:09 -0500

    docing it

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
-
+"""
+comples list of floats
+"""
 from typing import List
 
-def sum_list(input_list: List[float]) -> float:
 
+def sum_list(input_list: List[float]) -> float:
+    """
+    takes list of floats as args and returns sum as flaot
+    """
     return sum(input_list)
