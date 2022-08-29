0ff7fbada878493ae605f29c7994b0f8eb4a1f75
Cameron Crupper <guitardude20795@yahoo.com>
2022-08-29 17:04:24 -0500

    pycode and doc

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
+"""
+basic addition annotation
+"""
+
 
 def add(a: float, b: float) -> float:
-
+    """
+    takes 2 floats and return their sum
+    """
     return(a + b)
