00dbe5db8d9aca803f7f6a06a28969aeceffccac
Cameron Crupper <guitardude20795@yahoo.com>
2022-08-30 13:34:01 -0500

    takes code and alters to new function

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env pyhton3
+"""
+calling task_wait_random
+"""
+
+
+import asyncio
+import random
+from typing import List
+
+
+task_wait_random = __import__('3-tasks').wait_random
+
+
+async def task_wait_n(n: int = 0, max_delay: int = 10) -> List[float]:
+    """
+    takes code from wait_n and turns it into task_wait_n
+    """
+    pauses: List[float] = []
+    tasks: List[asyncio.Task] = []
+
+    for _ in range(n):
+        tasks.append(task_wait_random(max_delay))
+
+    for task in asyncio.as_completed((tasks)):
+        pause = await task
+        pauses.append(pause)
+
+    return pauses
