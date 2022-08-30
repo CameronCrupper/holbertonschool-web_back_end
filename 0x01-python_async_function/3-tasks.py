46a16e2ae4b292a4b205f2f9b7263cd99b2cfef7
Cameron Crupper <guitardude20795@yahoo.com>
2022-08-30 13:21:37 -0500

    wasnt annotating to asyncio.Task

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -11,7 +11,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int = 10):
+def task_wait_random(max_delay: int = 10) -> asyncio.Task:
     """
     max_delay is 10
     returns task thats been created
