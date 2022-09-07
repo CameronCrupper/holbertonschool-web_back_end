535c5150d26ecab1a76f85e79ec9bd990d4deb4d
Cameron Crupper <guitardude20795@yahoo.com>
2022-09-07 12:42:10 -0500

    lru cache

---
+++ 0x00-caching/3-lru_cache.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+LRU Caching
+"""
+
+
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """
+    
+    """
+    def __init__(self):
+        """
+        init
+        """
+        super().__init__()
+        self.leastrecent = []
+
+    def put(self, key, item):
+        """
+        
+        """
+        if key or item is not None:
+            valuecache = self.get(key)
+            if valuecache is None:
+                if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                    keydel = self.leastrecent
+                    lendel = len(keydel) - 1
+                    del self.cache_data[keydel[lendel]]
+                    print("DISCARD: {}".format(self.leastrecent.pop()))
+            else:
+                del self.cache_data[key]
+            if key in self.leastrecent:
+                self.leastrecent.remove(key)
+                self.leastrecent.insert(0, key)
+            else:
+                self.leastrecent.insert(0, key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        
+        """
+        valuecache = self.cache_data.get(key)
+        if valuecache:
+            self.leastrecent.remove(key)
+            self.leastrecent.insert(0, key)
+        return valuecache
