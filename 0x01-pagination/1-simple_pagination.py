001fab47fc2631e84e1bb3363fe2ff0092d05d66
Cameron Crupper <guitardude20795@yahoo.com>
2022-09-12 14:11:13 -0500

    test

--- 0x01-pagination/1-simple_pagination.py
+++ 0x01-pagination/1-simple_pagination.py
@@ -29,7 +29,8 @@ class Server:
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """
-        
+        takes two integer args page with default value 1
+        and page_size with default value 10
         """
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
