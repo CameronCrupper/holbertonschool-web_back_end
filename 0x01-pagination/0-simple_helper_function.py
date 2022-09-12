d8da3eb3b6a84b634535e3df7f27e8ce835fd1e4
Cameron Crupper <guitardude20795@yahoo.com>
2022-09-12 14:13:26 -0500

    test

--- 0x01-pagination/0-simple_helper_function.py
+++ 0x01-pagination/0-simple_helper_function.py
@@ -6,6 +6,7 @@ Simple Helper Function
 
 from typing import Tuple
 
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     returns tuple of size two that contains a start index and
