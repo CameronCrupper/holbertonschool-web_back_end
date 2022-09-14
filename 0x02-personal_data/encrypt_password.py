d6e4cc1725d34f093fd22fbd8cde40b81dc79abe
Cameron Crupper <guitardude20795@yahoo.com>
2022-09-14 14:47:43 -0500

    encrypting password

---
+++ 0x02-personal_data/encrypt_password.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Encrypting Passwords
+"""
+
+
+import bcrypt
+
+
+def hash_password(password: str = '') ->bytes:
+    """
+    expects one string arg name password and
+    returns a salted, hashed password, which
+    is a byte string
+    """
+    return bcrypt.hashpw(password.encode('utf-8'),bcrypt.gensalt())
