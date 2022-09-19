fb2c84e03cf1b111d71845b98435dd6c1aebcf22
Cameron Crupper <guitardude20795@yahoo.com>
2022-09-19 11:43:22 -0500

    test

--- 0x03-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x03-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,7 +5,9 @@
 from flask import request
 from typing import List, TypeVar
 from api.v1.auth.auth import Auth
-import base64
+import base64 import b64decode
+import re
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -17,4 +19,9 @@ class BasicAuth(Auth):
         if base64_authorization_header is None:
             return None
         if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            return b64decode(base64_authorization_header).decode('utf-8')
+
+        except Exception:
             return None
