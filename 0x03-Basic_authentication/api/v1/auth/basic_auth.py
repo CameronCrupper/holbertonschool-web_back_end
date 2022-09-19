f78a3d772fed38cfc94aa3e8c44311cb75cd3bad
Cameron Crupper <guitardude20795@yahoo.com>
2022-09-19 11:34:21 -0500

    test

--- 0x03-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x03-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,7 +5,16 @@
 from flask import request
 from typing import List, TypeVar
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """class to define authorization"""
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """
+        base64 decode
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
