103a0b71dd5b076d92124dedb12823e935360392
Cameron Crupper <guitardude20795@yahoo.com>
2022-09-19 15:09:13 -0500

    test

--- 0x03-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x03-Basic_authentication/api/v1/auth/basic_auth.py
@@ -13,6 +13,23 @@ from models.user import User
 class BasicAuth(Auth):
     """class to define authorization"""
 
+    def extract_base64_authorization_header(self, authorization_header: str):
+        """
+        returns base64
+        """
+        if authorization_header is None:
+            return None
+        if type(authorization_header) is not str:
+            return None
+        string = re.findall("Basic", authorization_header)
+        if string == []:
+            return None
+        else:
+            new = re.split(" ", authorization_header, 1)
+            if len(new) != 2:
+                return None
+            return new[1]
+
     def extract_user_credentials(self,
                                  decoded_base64_authorization_header:
                                  str) -> (str, str):
@@ -43,27 +60,27 @@ class BasicAuth(Auth):
         except Exception:
             return None
 
-    def user_object_from_credentials(self,
-                                     user_email: str,
+    def user_object_from_credentials(self, user_email: str,
                                      user_pwd: str) -> TypeVar('User'):
-        """Returns the User instance based on his email and password"""
+        """
+        user based on email and password
+        """
         if user_email is None or type(user_email) is not str:
             return None
         if user_pwd is None or type(user_pwd) is not str:
             return None
-
         try:
             search = User.search({'email': user_email})
-
             for user in search:
                 if user.is_valid_password(user_pwd):
                     return user
-
         except Exception:
             return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """Current user method"""
+        """
+        overloads auth and retrieves user
+        """
         auth = request.headers.get('Authorization')
         header = self.extract_base64_authorization_header(auth)
         decoded_header = self.decode_base64_authorization_header(header)
