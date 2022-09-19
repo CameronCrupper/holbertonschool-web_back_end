89ec1a3d9b5c54c9e70eda68a6d326a370c4ec1e
Cameron Crupper <guitardude20795@yahoo.com>
2022-09-19 12:08:36 -0500

    test

--- 0x03-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x03-Basic_authentication/api/v1/auth/basic_auth.py
@@ -43,6 +43,26 @@ class BasicAuth(Auth):
         except Exception:
             return None
 
+
+    def user_object_from_credentials(self,
+                                     user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """Returns the User instance based on his email and password"""
+        if user_email is None or type(user_email) is not str:
+            return None
+        if user_pwd is None or type(user_pwd) is not str:
+            return None
+
+        try:
+            search = User.search({'email': user_email})
+
+            for user in search:
+                if user.is_valid_password(user_pwd):
+                    return user
+
+        except Exception:
+            return None
+
     def current_user(self, request=None) -> TypeVar('User'):
         """Current user method"""
         auth = request.headers.get('Authorization')
