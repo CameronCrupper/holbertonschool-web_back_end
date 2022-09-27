01d14886d93c75186652d264f93461794367539c
Cameron Crupper <guitardude20795@yahoo.com>
2022-09-27 13:02:51 -0500

    test

--- 0x05-user_authentication_service/auth.py
+++ 0x05-user_authentication_service/auth.py
@@ -6,6 +6,7 @@ from bcrypt import hashpw, gensalt
 from db import DB
 from user import User
 from uuid import uuid4
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
@@ -13,3 +14,24 @@ def _hash_password(password: str) -> bytes:
     hash password
     """
     return hashpw(password.encode('utf8'), gensalt())
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+            Register User
+        """
+        try:
+            consult = self._db.find_user_by(email=email)
+            raise ValueError(f'<{consult.email}> already exists.')
+
+        except NoResultFound:
+            passwd: str = _hash_password(password)
+            user = self._db.add_user(email, passwd)
+
+        return user
