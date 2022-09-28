2b2eba0f94931ce532645a1cae407bfb9deb8540
Cameron Crupper <guitardude20795@yahoo.com>
2022-09-27 21:53:58 -0500

    test

--- 0x05-user_authentication_service/auth.py
+++ 0x05-user_authentication_service/auth.py
@@ -100,17 +100,12 @@ class Auth:
 
 def get_reset_password_token(self, email: str) -> str:
         """
-        finds user corresponding to the email
+        fins corresponding email and generates uuid
         """
-        if email is None:
-            raise ValueError
-
         try:
             user = self._db.find_user_by(email=email)
-
-            token: str = _generate_uuid()
-            self._db.update_user((user.id), reset_token=token)
-
-            return token
-        except (NoResultFound, InvalidRequestError):
+        except NoResultFound:
             raise ValueError
+        user.reset_token = str(uuid4())
+        return user.reset_token
+
