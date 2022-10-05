99e8356e42d9ddd4f7004610bf1b400e70a56f62
Cameron Crupper <guitardude20795@yahoo.com>
2022-10-05 10:19:13 -0500

    test

---
+++ 0x01-i18n/2-app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+Basic Babel setup
+"""
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask_babel import Babel
+
+
+class Config(object):
+    """
+    Configuration for babel
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_DEFAULT_LOCALE = 'en'
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+@babel.localeselector
+def get_locale():
+    """
+    
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """
+    return simple greeting
+    """
+    return render_template('1-index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
