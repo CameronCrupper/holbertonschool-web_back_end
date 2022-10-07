8708dc671d2c5be3b4b5de6743746ef1de7d9071
Cameron Crupper <guitardude20795@yahoo.com>
2022-10-07 13:27:28 -0500

    test

--- 0x00-Unittests_and_integration_tests/test_utils.py
+++ 0x00-Unittests_and_integration_tests/test_utils.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-
+Test Utils
 """
 import unittest
 import math
@@ -12,9 +12,8 @@ import requests
 
 class TestAccessNestedMap(unittest.TestCase):
     """
-
+    Access Nested Map
     """
-
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
@@ -23,7 +22,7 @@ class TestAccessNestedMap(unittest.TestCase):
 
     def test_access_nested_map(self, nested_map, path_map, result_expec):
         """
-
+        test access nested map
         """
         self.assertEqual(access_nested_map(nested_map, path_map), result_expec)
 
@@ -33,7 +32,7 @@ class TestAccessNestedMap(unittest.TestCase):
     ])
     def test_access_nested_map_exception(self, nested_map, path_map):
         """
-
+        test access nested map acception
         """
         with self.assertRaises(KeyError) as error:
             access_nested_map(nested_map, path_map)
@@ -42,7 +41,7 @@ class TestAccessNestedMap(unittest.TestCase):
 
 class TestGetJson(unittest.TestCase):
     """
-    
+    test get json
     """
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
@@ -50,7 +49,7 @@ class TestGetJson(unittest.TestCase):
     ])
     def test_get_json(self, test_url, test_payload):
         """
-        
+        test get json
         """
         with patch('request.get') as mock_request:
             mock_request.return_value.json.return_value = test_payload
@@ -58,25 +57,25 @@ class TestGetJson(unittest.TestCase):
 
 class TestMemoize(unittest.TestCase):
     """
-    
+    test memoize
     """
     def test_memoize(self):
         """
-        
+        test memoize
         """
         class TestClass:
             """
-            
+            test class
             """
             def a_method(self):
                 """
-                
+                a method
                 """
                 return 42
             @memoize
             def a_property(self):
                 """
-                
+                a property
                 """
                 return self.a_method()
 
