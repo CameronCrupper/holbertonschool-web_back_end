0489f58a461c633a7543122eeb355f5865f5a8bc
Cameron Crupper <guitardude20795@yahoo.com>
2022-10-07 13:32:50 -0500

    test

--- 0x00-Unittests_and_integration_tests/test_utils.py
+++ 0x00-Unittests_and_integration_tests/test_utils.py
@@ -83,7 +83,7 @@ class TestMemoize(unittest.TestCase):
                 return self.a_method()
 
         with patch.object(TestClass, 'a_method') as mock:
-            test_class =TestClass()
+            test_class = TestClass()
             test_class.a_property()
             test_class.a_property()
             mock.assert_called_once()
