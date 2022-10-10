cbc8564fc59971697cd15487f968ed3103b5b52d
Cameron Crupper <guitardude20795@yahoo.com>
2022-10-10 15:47:36 -0500

    test files and main exercise file

---
+++ 0x00-redis_basic/exercise.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env
+"""
+Redis Basics
+"""
+import redis
+from uuid import uuid4
+from typing import Union, Callable
+from functools import wraps
+
+
+def count_calls(method: Callable = None) -> Callable:
+    """
+    decorator that counts calls
+    """
+    name = method.__qualname__
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """
+        wrapper method as described
+        https://docs.python.org/3.7/library/functools.html#functools.wraps
+        """
+        self._redis.incr(name)
+        return method(self, *args, **kwargs)
+    return wrapper
+
+def call_history(method: Callable) -> Callable:
+    """
+    Call History Decorator
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """
+        wrapper function
+        """
+        input: str = str(args)
+        self._redis.rpush(method.__qualname__ + ":inputs", input)
+        output = str(method(self, *args, **kwargs))
+        self._redis.rpush(method.__qualname__ + ":outputs", output)
+        return output
+    return wrapper
+
+def replay(func: Callable):
+    """
+    replay function
+    """
+    r = redis.Redis()
+    func_name = func.__qualname__
+    number_calls = r.get(func_name)
+    try:
+        number_calls = number_calls.decode('UTF-8')
+    except Exception:
+        number_calls = 0
+    print(f'{func_name} was called {number_calls} times:')
+
+    ins = r.lrange(func_name + ":inputs", 0, -1)
+    outs = r.lrange(func_name + ":outputs", 0, -1)
+
+    for cin, cout in zip(ins, outs):
+        try:
+            cin = cin.decode('UTF-8')
+        except Exception:
+            cin = ""
+        try:
+            cout = cout.decode('UTF-8')
+        except Exception:
+            cout = ""
+        print(f'{func_name}(*{cin}) -> {cout}')
+
+
+class Cache():
+    """
+    Redis Functionality
+    """
+    def __init__(self):
+        """
+        Constructor
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+    @call_history
+    @count_calls
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        Stores cache
+        """
+        key = str(uuid4())
+        self._redis.set(key, data)
+        return key
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
+        """
+        reading from redis and recovering original type
+        """
+        key = self._redis.get(key)
+        if fn:
+            return fn(key)
+        return key
+
