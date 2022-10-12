d537c001e325b788732ff7a8088eabf879748351
Cameron Crupper <guitardude20795@yahoo.com>
2022-10-12 10:25:07 -0500

    push 3

---
+++ 0x01-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,8 @@
+--Import this table dump: metal_bands.sql.zip
+--Column names must be: band_name and lifespan (in years)
+--You should use attributes formed and split for computing the lifespan
+--Your script can be executed on any database
+
+SELECT band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%';
