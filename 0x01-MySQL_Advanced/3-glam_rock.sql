5d508f314b11a65ab4f3e028530fa1adef0251e1
Cameron Crupper <guitardude20795@yahoo.com>
2022-10-12 10:29:59 -0500

    test for bigger numbers

--- 0x01-MySQL_Advanced/3-glam_rock.sql
+++ 0x01-MySQL_Advanced/3-glam_rock.sql
@@ -3,6 +3,6 @@
 --You should use attributes formed and split for computing the lifespan
 --Your script can be executed on any database
 
-SELECT band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
+SELECT band_name, (IFNULL(split, 2020) -  formed) AS lifespan
 FROM metal_bands
-WHERE style LIKE '%Glam rock%';
+WHERE style REGEXP 'Glam rock' ORDER BY lifespan DESC;
