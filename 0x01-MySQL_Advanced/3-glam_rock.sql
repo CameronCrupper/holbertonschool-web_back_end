641636e381815c989d382a8d2ed27196273fa269
Cameron Crupper <guitardude20795@yahoo.com>
2022-10-12 10:41:32 -0500

    maybe fixed

--- 0x01-MySQL_Advanced/3-glam_rock.sql
+++ 0x01-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,7 @@
---Import this table dump: metal_bands.sql.zip
---Column names must be: band_name and lifespan (in years)
---You should use attributes formed and split for computing the lifespan
---Your script can be executed on any database
+-- Import this table dump: metal_bands.sql.zip
+-- Column names must be: band_name and lifespan (in years)
+-- You should use attributes formed and split for computing the lifespan
+-- Your script can be executed on any database
 
 SELECT band_name, (IFNULL(split, 2020) -  formed) AS lifespan
 FROM metal_bands
