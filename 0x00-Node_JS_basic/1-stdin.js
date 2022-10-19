1fa819e5e2f8359cba5a6188df02cd788b96ac95
Cameron Crupper <guitardude20795@yahoo.com>
2022-10-19 09:30:15 -0500

    command line

---
+++ 0x00-Node_JS_basic/1-stdin.js
@@ -0,0 +1,11 @@
+
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdin.on("readable", () => {
+  const userName = process.stdin.read();
+  if (userName !== null) {
+    process.stdout.write(`Your name is: ${userName}`);
+  }
+});
+process.on("exit", () => {
+  process.stdout.write("This important software is now closing\n");
+});
