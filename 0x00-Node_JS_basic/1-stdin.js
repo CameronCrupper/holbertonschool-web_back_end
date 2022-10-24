ae4bce32c261e681fbe96e9a9b27dbee887edc65
Cameron Crupper <guitardude20795@yahoo.com>
2022-10-24 11:39:49 -0500

    fixiing eslint

--- 0x00-Node_JS_basic/1-stdin.js
+++ 0x00-Node_JS_basic/1-stdin.js
@@ -1,11 +1,10 @@
-
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
-process.stdin.on("readable", () => {
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('readable', () => {
   const userName = process.stdin.read();
   if (userName !== null) {
     process.stdout.write(`Your name is: ${userName}`);
   }
 });
-process.on("exit", () => {
-  process.stdout.write("This important software is now closing\n");
+process.on('exit', () => {
+  process.stdout.write('This important software is now closing\n');
 });
