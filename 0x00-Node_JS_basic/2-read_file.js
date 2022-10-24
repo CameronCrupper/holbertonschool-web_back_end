57481cd28260b3cea95e6c12148720d1b8c31bc6
Cameron Crupper <guitardude20795@yahoo.com>
2022-10-24 11:43:10 -0500

    first push

---
+++ 0x00-Node_JS_basic/2-read_file.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+module.exports = function countStudents(path) {
+  try {
+    const field = {};
+    const data = fs.readFileSync(path, 'UTF-8');
+    const students = data.split('\n').map((student) => student.split(','));
+    students.shift();
+    console.log(`Number of students: ${students.length}`);
+    students.forEach((student) => {
+      if (!field[student[3]]) field[student[3]] = [];
+      field[student[3]].push(student[0]);
+    });
+    Object.keys(field).forEach((key) => {
+      console.log(`Number of students in ${key}: ${field[key].length}. List: ${field[key].join(', ')}`);
+    });
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+};
---
+++ 0x00-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+module.exports = async function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'UTF-8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+      const students = data.split('\n').map((student) => student.split(','));
+      const field = {};
+      students.shift();
+      const message = [];
+      message.push(`Number of students: ${students.length}`);
+      students.forEach((student) => {
+        if (!field[student[3]]) field[student[3]] = [];
+        field[student[3]].push(student[0]);
+      });
+      Object.keys(field).forEach((key) => {
+        message.push(`Number of students in ${key}: ${field[key].length}. List: ${field[key].join(', ')}`);
+      });
+      const result = message.join('\n');
+      console.log(result);
+      resolve(result);
+    });
+  });
+};
