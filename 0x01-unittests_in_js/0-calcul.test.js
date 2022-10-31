f2da47f1ee2511a31f8ebce06455e33a325692f7
Cameron Crupper <guitardude20795@yahoo.com>
2022-10-31 10:05:05 -0500

    test

--- 0x01-unittests_in_js/0-calcul.test.js
+++ 0x01-unittests_in_js/0-calcul.test.js
@@ -1,23 +1,17 @@
+/* eslint-disable jest/prefer-expect-assertions */
+/* eslint-disable jest/expect-expect */
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
-  it('checks the output', function () {
+  // eslint-disable-next-line jest/prefer-expect-assertions
+  it('postive intergers', function () {
     assert.strictEqual(calculateNumber(1, 3), 4);
     assert.strictEqual(calculateNumber(1, 3.7), 5);
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
     assert.strictEqual(calculateNumber(3.7, 1), 5);
     assert.strictEqual(calculateNumber(3.7, 1.2), 5);
-  });
-  it('negative numbers', () => {
-    assert.strictEqual(calculateNumber(-1, 1), 0);
-    assert.strictEqual(calculateNumber(1, -1), 0);
-    assert.strictEqual(calculateNumber(-1, -1), -2);
-  });
-  it('checks arguments', () => {
-    assert.strictEqual(isNaN(calculateNumber(1)), true);
-    assert.strictEqual(isNaN(calculateNumber()), true);
   });
   // eslint-disable-next-line jest/prefer-expect-assertions
   it('negative intergers', () => {
@@ -28,6 +22,7 @@ describe('calculateNumber', () => {
     assert.strictEqual(calculateNumber(-3.7, -1), -5);
     assert.strictEqual(calculateNumber(-3.7, -1.2), -5);
   });
+
   it('returns NaN if a or b is infinite', () => {
     assert.strictEqual(calculateNumber(Infinity, 1.6), Infinity);
     assert.strictEqual(calculateNumber(1.6, Infinity), Infinity);
@@ -40,5 +35,5 @@ describe('calculateNumber', () => {
     assert.strictEqual(calculateNumber('A', 1.6), NaN);
     assert.strictEqual(calculateNumber(1.6, 'B'), NaN);
     assert.strictEqual(calculateNumber('A', 'B'), NaN);
-});
+  });
 });
